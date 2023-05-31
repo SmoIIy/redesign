0966c33e155acacf4eae67ac4c83a2f8206391ba
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2023-05-31 16:44:02 +0200

    burgermenu impl

--- menu.js
+++ menu.js
@@ -2,3 +2,13 @@ function toggleMenu() {
   var menu = document.getElementById("menu");
   menu.classList.toggle("active");
 }
+/* Hamburger Menu */
+
+window.onload = function (){
+  const menuBtn = document.querySelector('.hamburger');
+  const mobileMenu = document.querySelector('.mobile-nav-links');
+  window.addEventListener('click', function(){
+      menuBtn.classList.toggle('is-active')
+      mobileMenu.classList.toggle('is-active')
+  });
+}
